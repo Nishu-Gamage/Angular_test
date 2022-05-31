260c3200138146bfa6aaa874fe1a2fded5672db4
Nishu-Gamage <nishuchatu@gmail.com>
2022-05-31 22:22:00 +0900

    2022/5/31

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -6,5 +6,32 @@ import { Component } from "@angular/core";
      styleUrls: ['./success-alert.component.css']
    })
 export class SuccessAlert{
+  
+  disableTest = false;
+  test:string = "Success alert works!";
+  test1:number = 10;
+  test2:string ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis atque quia cum excepturi facilis aliquam accusamus dignissimos ullam quis eum quos voluptas perspiciatis itaque in et nam, eaque earum nihil!"
+  
+  nishu = '';
+
+    constructor(){
+      // setTimeout(()=>{
+      //   this.disableTest = true;
+      // }, 2000)
+    }
+
+    getAge(){
+      return 20;
+    }
+
+    btnDisabled(){
+      this.disableTest = !this.disableTest;
+    }
+
+    onUpdateText(event:any){
+      console.log(event);
+      this.nishu = event.target.value;
+    }
+
 
 }
